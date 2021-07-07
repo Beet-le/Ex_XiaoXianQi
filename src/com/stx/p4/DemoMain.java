d3bc91726abd0b026f5d05a377735d160b01e67c
Bee-le <1693332907@qq.com>
2021-07-07 15:05:55 +0800

    四题

---
+++ src/com/stx/p4/DemoMain.java
@@ -0,0 +1,8 @@
+package com.stx.p4;
+
+public class DemoMain {
+    public static void main(String[] args) {
+        Noodle noodle=new Noodle("羊肉面",2,true);
+//        Noodle n1 = new Noodle("羊肉面", 2 ) ;
+    }
+}
---
+++ src/com/stx/p4/Noodle.java
@@ -0,0 +1,37 @@
+package com.stx.p4;
+
+public class Noodle {
+    private String  type;
+    private int weight;
+    private boolean isHaveSoup;
+
+    public Noodle(String type, int weight, boolean isHaveSoup) {
+        this.type = type;
+        this.weight = weight;
+        this.isHaveSoup = isHaveSoup;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public boolean isHaveSoup() {
+        return isHaveSoup;
+    }
+
+    public void setHaveSoup(boolean haveSoup) {
+        isHaveSoup = haveSoup;
+    }
+}
