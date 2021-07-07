aea646b499e69f7d1bc1de4f9669e33b14f193b0
Bee-le <1693332907@qq.com>
2021-07-07 16:50:39 +0800

    五题

--- src/com/stx/p5/Demo01.java
+++ src/com/stx/p5/Demo01.java
@@ -3,28 +3,28 @@ package com.stx.p5;
 import java.util.Scanner;
 
 public class Demo01 {
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            int n = sc.nextInt();
-            int m = sc.nextInt();
-            char one = 'A';
-            char[][] arr = new char[n][m];
-            for(int j = 0;j<m;j++) {
-                arr[0][j] = (char)(one +j);
-            }
-            for (int i = 0;i<n;i++){
-                arr[i][0] = (char) (one + i);
-            }
-            for (int x = 1;x<n;x++){
-                for (int y = 1;y<m;y++){
-                    arr[x][y] = arr[x-1][y-1];
-                }
-            }
-            for (int a = 0;a<n;a++){
-                for (int b = 0;b<m;b++){
-                    System.out.print(arr[a][b]);
-                }
-                System.out.println();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        char one = 'A';
+        char[][] arr = new char[n][m];
+        for (int j = 0; j < m; j++) {
+            arr[0][j] = (char) (one + j);
+        }
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = (char) (one + i);
+        }
+        for (int x = 1; x < n; x++) {
+            for (int y = 1; y < m; y++) {
+                arr[x][y] = arr[x - 1][y - 1];
             }
         }
+        for (int a = 0; a < n; a++) {
+            for (int b = 0; b < m; b++) {
+                System.out.print(arr[a][b]);
+            }
+            System.out.println();
+        }
+    }
 }
