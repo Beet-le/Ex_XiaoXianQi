95f7f0c7f6d3e7c436b7ee6b430d6958720970b2
Bee-le <1693332907@qq.com>
2021-07-07 17:08:30 +0800

    五题

--- src/com/stx/p5/Demo01.java
+++ src/com/stx/p5/Demo01.java
@@ -9,17 +9,21 @@ public class Demo01 {
         int m = sc.nextInt();
         char one = 'A';
         char[][] Array = new char[n][m];
-        for (int j = 0; j < m; j++) {
-            Array[0][j] = (char) (one + j);
-        }
+
         for (int i = 0; i < n; i++) {
             Array[i][0] = (char) (one + i);
         }
+
+        for (int j = 0; j < m; j++) {
+            Array[0][j] = (char) (one + j);
+        }
+
         for (int x = 1; x < n; x++) {
             for (int y = 1; y < m; y++) {
                 Array[x][y] = Array[x - 1][y - 1];
             }
         }
+
         for (int a = 0; a < n; a++) {
             for (int b = 0; b < m; b++) {
                 System.out.print(Array[a][b]);
